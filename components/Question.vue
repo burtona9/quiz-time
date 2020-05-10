7181d2eb7de0a97bbd6063228846d727be786f15
Adam Burton <adamburton27@hotmail.com>
2020-05-10 17:09:30 +0100

    commit

---
+++ components/Question.vue
@@ -0,0 +1,24 @@
+<template>
+<div>
+    <p>{{question}}</p>
+    <div v-for="(option,index) in options" :key="index" on:click="onOptionSelected">
+        {{option.text}}
+    </div>
+</div>
+</template>
+<script>
+export default {
+    props:{
+        question: String,
+        options: Array
+    },
+    methods:{
+        onOptionSelected(){
+            this.$emit("onOptionSelected");
+        }
+    }
+}
+</script>
+<style>
+
+</style>
---
+++ components/Quiz.vue
@@ -0,0 +1,23 @@
+<template>
+<div>
+    <p>{{questions}}</p>
+    
+</div>
+</template>
+<script>
+export default {
+    data: function(){
+        return {
+            questions = this.$store.questions
+        }
+    },
+    methods:{
+        onOptionSelected(){
+            this.$emit("onOptionSelected");
+        }
+    }
+}
+</script>
+<style>
+
+</style>
